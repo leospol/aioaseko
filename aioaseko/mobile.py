0f71e5a7f61e4ecd16cfac5f7a640c9e60967714
Leos Pol <lpol@redhat.com>
2023-07-04 19:07:50 +0200

    refresh_token support added

--- aioaseko/mobile.py
+++ aioaseko/mobile.py
@@ -18,8 +18,10 @@
 """aioAseko mobile API account."""
 from __future__ import annotations
 
-from typing import TYPE_CHECKING
+import time
+import jwt
 
+from typing import TYPE_CHECKING
 from aiohttp import ClientError
 
 from .exceptions import APIUnavailable, InvalidAuthCredentials
@@ -38,22 +40,31 @@ class MobileAccount:
         username: str | None = None,
         password: str | None = None,
         access_token: str | None = None,
+        refresh_token: str | None = None,
     ) -> None:
         """Init Aseko account."""
         self._session = session
         self._username = username
         self._password = password
         self._access_token = access_token
+        self._refresh_token = refresh_token
 
     @property
     def access_token(self) -> str | None:
         """Return access token."""
         return self._access_token
 
+    @property
+    def refresh_token(self) -> str | None:
+        """Return refresh token."""
+        return self._refresh_token
+
     async def _request(
-        self, method: str, path: str, data: dict | None = None
+        self, method: str, path: str, data: dict | None = None, refresh: bool = False
     ) -> ClientResponse:
         """Make a request to the Aseko mobile API."""
+        if not refresh and not self._is_access_token_valid() and self._refresh_token is not None:
+            await self.refresh()
         resp = await self._session.request(
             method,
             f"https://pool.aseko.com/api/v1/{path}",
@@ -83,10 +94,34 @@ class MobileAccount:
         )
         data = await resp.json()
         self._access_token = data["accessToken"]
+        self._refresh_token = data["refreshToken"]
 
     async def logout(self) -> None:
         """Logout Aseko Pool Live account."""
         await self._request("post", "logout")
+
+    def _is_access_token_valid(self):
+        """Check is access token is still valid"""
+        if self._access_token is not None:
+            payload = jwt.decode(self._access_token, "", ["RS256"], options={"verify_signature": False})
+            return "exp" in payload and time.time() < payload["exp"]
+        return False
+
+    async def refresh(self) -> None:
+        """Refresh access token."""
+        # invalidate the current access token
+        self._access_token = None
+        resp = await self._request(
+            "post",
+            "refresh",
+            {
+                "refreshToken": self.refresh_token
+            },
+            refresh=True
+        )
+        data = await resp.json()
+        self._access_token = data["accessToken"]
+        self._refresh_token = data["refreshToken"]
 
     async def get_units(self) -> list[Unit]:
         """Get units."""
